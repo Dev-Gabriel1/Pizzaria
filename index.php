4e7e8682d6338e87b68431c6bf160d74da985c22
gabriel <mooneyadriel@gmail.com>
2021-12-20 17:15:33 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        Maricota
+</title>
+<link rel="icon" 
+      type="image/jpg" 
+      href="icon-pizza.png" />
+</head>
+    <link href="css.css" rel="stylesheet" >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<header>
+<div class="container-fluid bg-black">
+  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+      <span class="fs-4 d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none"><a href="index.php"><img class ="logo" src="logo.jpg"></a>Maricota Pizzaria e Hamburgueria</span>
+      <ul class="nav">
+        <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Promoções</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Contatos</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Cadastrar</a></li>
+      </ul>
+</div>
+<header>
+<main class="corpo">
+<div class="album py-5 bg-light">
+  <div class="container">
+
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+      <div class="col">
+        <div class="card shadow-sm">
+          <img width="100%" height="225" src="fotos\xsalada.jpg">
+
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Adicinonar ao Carrinho</button>
+              </div>
+              <small class="valor" >R$ 39,99</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-sm">
+        <img width="100%" height="225" src="fotos\xbacon.jpg">
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Adicinonar ao Carrinho</button>
+              </div>
+              <small class="valor">R$ 39,99</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-sm">
+        <img width="100%" height="225" src="fotos\xtudo.jpg">
+
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Adicinonar ao Carrinho</button>
+              </div>
+              <small class="valor">R$ 39,99</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card shadow-sm">
+        <img width="100%" height="225" src="fotos\pizza-calabresa.jpg">
+
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Adicinonar ao Carrinho</button>
+              </div>
+              <small class="valor">R$ 39,99</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-sm">
+        <img width="100%" height="225" src="fotos\pizza-charque.jpg">
+
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Adicinonar ao Carrinho</button>
+              </div>
+              <small class="valor">R$ 39,99</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-sm">
+        <img width="100%" height="225" src="fotos\pizza-camarao.jpg">
+
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Adicinonar ao Carrinho</button>
+              </div>
+              <small class="valor">R$ 39,99</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card shadow-sm">
+        <img width="100%" height="225" src="fotos\lata.png">
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Adicinonar ao Carrinho</button>
+              </div>
+              <small class="valor">R$ 39,99</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-sm">
+        <img width="100%" height="225" src="fotos\guarana.jpg">
+
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Adicinonar ao Carrinho</button>
+              </div>
+              <small class="valor">R$ 39,99</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-sm">
+        <img width="100%" height="225" src="fotos\coca-cola.jpg">
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Adicinonar ao Carrinho</button>
+              </div>
+              <small class="valor">R$ 37,99</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
